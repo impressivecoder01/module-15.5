524b3675f70fe83c7ff01f3f222b1325bfffe284
impressivecoder1 <rohanrahman251@gmail.com>
2025-10-03 02:00:11 +0600

    class4done

--- class4.js
+++ class4.js
@@ -8,3 +8,16 @@ numbers.push(333)
 console.log(numbers)
 //----------pop
 const friends = ['balam', 'kalam', 'salam', 'gelam']
+friends.pop();
+console.log(friends)
+const out2 = friends.pop();
+console.log(out2)
+
+//-----------
+const friend = ['balam', 'kalam', 'salam', 'gelam'];
+friend.shift();
+console.log(friend);
+friend.unshift('bd.com')
+console.log(friend)
+friend.unshift('bd.com', 'bd')
+console.log(friend)
