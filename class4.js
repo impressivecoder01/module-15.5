93748ac4ed3d0dcefb9746e681e98e52d2c758d1
impressivecoder1 <rohanrahman251@gmail.com>
2025-10-02 23:52:02 +0600

    class4

---
+++ class4.js
@@ -0,0 +1,10 @@
+// add/remove elements from arrays using push, pop shift and unshift
+//--------push
+let ages = [];
+ages = [33];
+console.log(ages)
+const numbers = [33,32,24,565,54,35,67];
+numbers.push(333)
+console.log(numbers)
+//----------pop
+const friends = ['balam', 'kalam', 'salam', 'gelam']
