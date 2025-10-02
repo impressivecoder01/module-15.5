f575fe3f0119929a4887e19ca32bcf466efc3456
impressivecoder1 <rohanrahman251@gmail.com>
2025-10-02 23:40:20 +0600

    array index and set or update element value by index

--- class3.js
+++ class3.js
@@ -1,1 +1,14 @@
 // array length, index, get and set by index
+const num = [1,2, 3, 4, 5, 67,4]
+console.log(num.length)
+//--------------index.js
+const numbers = [3,44,56,745,356,77,543,22,2,11,2365,77];
+console.log(numbers[2])
+const forth = numbers[3]
+console.log(forth)
+//-----------set or update element value by index
+let abc = 45;
+abc = 33;
+console.log(abc);
+numbers[3] = 30;
+console.log(numbers)
